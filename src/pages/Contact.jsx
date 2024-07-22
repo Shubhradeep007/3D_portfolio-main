97608185e376c32fd2fcb3f91c54787e3920cf92
Shubhradeep Bose <shubhradeepbose79@gmail.com>
2024-07-23 02:28:59 +0530

    Update Contact.jsx
    updating contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -31,9 +31,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Shubhradeep",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "Shubhradeepbose79@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
@@ -89,7 +89,7 @@ const Contact = () => {
               type='text'
               name='name'
               className='input'
-              placeholder='John'
+              placeholder='Shubho'
               required
               value={form.name}
               onChange={handleChange}
@@ -103,7 +103,7 @@ const Contact = () => {
               type='email'
               name='email'
               className='input'
-              placeholder='John@gmail.com'
+              placeholder='Shubho@gmail.com'
               required
               value={form.email}
               onChange={handleChange}
